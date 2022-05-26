f9d4f7e2400ee4da3b5dea8baee1c3583a39c1dd
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-05-26 17:30:48 -0300

    style new

--- slick.js
+++ slick.js
@@ -1,7 +1,30 @@
+var root = document.documentElement;
+root.className += ' js';
 
-$('.carousel').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4
-  });
+function boxTop(idBox) {
+	var boxOffset = $(idBox).offset().top;
+	return boxOffset;
+}
+
+$(document).ready(function() {
+	var $target = $('.anime'),
+			animationClass = 'anime-init',
+			windowHeight = $(window).height(),
+			offset = windowHeight - (windowHeight / 4);
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+		$target.each(function() {
+			if (documentTop > boxTop(this) - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+	animeScroll();
+
+	$(document).scroll(function() {
+		setTimeout(function() {animeScroll()}, 150);
+	});
+});
